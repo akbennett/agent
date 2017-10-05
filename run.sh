a53bf9888026c076f99f9f4c2ac7473bf2974221
Tyler Baker <tyler@opensourcefoundries.com>
2017-10-05 13:48:14 -0700

    More improvements
    Signed-off-by: Tyler Baker <tyler@opensourcefoundries.com>

--- run.sh
+++ run.sh
@@ -2,15 +2,15 @@
 
 function update_agent {
 	echo "Checking current agent image sha..."
-	docker_current_sha=$(docker inspect "opensourcefoundries/agent:latest" | grep Id | sed "s/\"//g" | sed "s/,//g" |  tr -s ' ' | cut -d ' ' -f3)
-	echo "Current agent sha: $docker_current_sha"
+	echo "Current agent sha: $(cat /tmp/agent.sha)"
 	echo "Checking for new agent container..."
 	docker pull opensourcefoundries/agent:latest
-	docker_new_sha=$(docker inspect "opensourcefoundries/agent:latest" | grep Id | sed "s/\"//g" | sed "s/,//g" |  tr -s ' ' | cut -d ' ' -f3)
-	if [ "$docker_current_sha" != "$docker_new_sha" ] ; then
+	agent_current_sha=$(cat /tmp/agent.sha)
+	agent_new_sha=$(docker inspect "opensourcefoundries/agent:latest" | grep Id | sed "s/\"//g" | sed "s/,//g" |  tr -s ' ' | cut -d ' ' -f3)
+	if [ "$agent_current_sha" != "$agent_new_sha" ] ; then
 		echo "New agent container detected..."
 		echo "Restarting updated agent container..."
-		docker run -d -v /var/run/docker.sock:/var/run/docker.sock --name=agent_new --rm opensourcefoundries/agent $1 $2
+		docker run -d -v /var/run/docker.sock:/var/run/docker.sock --name=agent_new --rm opensourcefoundries/agent:latest $1 $2
 		docker stop agent
 	else
 		echo "No new agent container..."
@@ -18,7 +18,8 @@ function update_agent {
 }
 
 function update {
-	echo $1 > /tmp/current.sha
+	echo "TEST"
+	echo $1 > /tmp/git.sha
 	echo "Bringing down current container set..."
 	docker-compose down
 	echo "Deleteing current container set..."
@@ -35,14 +36,14 @@ rm -rf tmp
 sha=($(git ls-remote $repo refs/heads/$branch))
 git clone $repo -b $branch tmp
 cd tmp
-echo "Current sha: $sha"
+echo "Current GIT sha: $sha"
 update $sha
 
 while true
 do
 	sha=($(git ls-remote $repo refs/heads/$branch))
-	echo "Current sha: $sha"
-	current=$(cat /tmp/current.sha)
+	echo "Current GIT sha: $sha"
+	current=$(cat /tmp/git.sha)
 	if [ "$sha" != "$current" ] ; then
 		git pull
 		echo "New deployment found, updating..."
@@ -54,6 +55,8 @@ do
 			echo "Found new agent container, renaming..."
 			docker rename agent_new agent
 		fi
+		agent_current_sha=$(docker inspect "opensourcefoundries/agent:latest" | grep Id | sed "s/\"//g" | sed "s/,//g" |  tr -s ' ' | cut -d ' ' -f3)
+		echo $agent_current_sha > /tmp/agent.sha
 		update_agent $repo $branch
 		sleep 20
 	fi
