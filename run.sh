cd070efa34a9bd81df104cb26e0d01df12639bff
Tyler Baker <tyler@opensourcefoundries.com>
2017-10-05 19:23:14 -0700

    getting closer
    Signed-off-by: Tyler Baker <tyler@opensourcefoundries.com>

--- run.sh
+++ run.sh
@@ -1,32 +1,27 @@
 #!/bin/bash
 
 function update_agent {
-	echo "Checking current agent image sha..."
-	echo "Current agent sha: $(cat /tmp/agent.sha)"
-	echo "Checking for new agent container..."
-	docker pull opensourcefoundries/agent:latest
-	agent_current_sha=$(cat /tmp/agent.sha)
-	agent_new_sha=$(docker inspect "opensourcefoundries/agent:latest" | grep Id | sed "s/\"//g" | sed "s/,//g" |  tr -s ' ' | cut -d ' ' -f3)
-	if [ "$agent_current_sha" != "$agent_new_sha" ] ; then
-		echo "New agent container detected..."
-		echo "Restarting updated agent container..."
+	if docker pull opensourcefoundries/agent:latest | grep 'Downloaded newer image'
+	then
+		echo "AGENT: Update detected..."
+		echo "AGENT: Starting updated agent container..."
 		docker run -d -v /var/run/docker.sock:/var/run/docker.sock --name=agent_new --rm opensourcefoundries/agent:latest $1 $2
+		echo "AGENT: This agent will self destruct in..5..4..3..2..1.."
 		docker stop agent
 	else
-		echo "No new agent container..."
+		echo "AGENT: No new agent container..."
 	fi
 }
 
 function update {
-	echo "TEST"
 	echo $1 > /tmp/git.sha
-	echo "Bringing down current container set..."
+	echo "DOCKER-COMPOSE: Bringing down current container set..."
 	docker-compose down
-	echo "Deleteing current container set..."
+	echo "DOCKER-COMPOSE: Deleteing current container set..."
 	docker-compose rm -f
-	echo "Updating current container set..."
+	echo "DOCKER-COMPOSE: Updating current container set..."
 	docker-compose pull
-	echo "Bringing up current container set.."
+	echo "DOCKER-COMPOSE: Bringing up current container set.."
 	docker-compose up -d
 }
 
@@ -36,27 +31,35 @@ rm -rf tmp
 sha=($(git ls-remote $repo refs/heads/$branch))
 git clone $repo -b $branch tmp
 cd tmp
-echo "Current GIT sha: $sha"
-update $sha
+echo "INIT: TRIGGER: SHA: $sha"
+if docker ps | grep agent_new > /dev/null
+then
+	echo "INIT: UPDATE: Agent updated..."
+	echo $sha > /tmp/git.sha
+else
+	echo "INIT: FIRSTBOOT: Updating current container set..."
+	update $sha
+fi
 
 while true
 do
 	sha=($(git ls-remote $repo refs/heads/$branch))
-	echo "Current GIT sha: $sha"
+	echo "LOOP: TRIGGER: SHA: $sha"
 	current=$(cat /tmp/git.sha)
 	if [ "$sha" != "$current" ] ; then
 		git pull
-		echo "New deployment found, updating..."
+		echo "LOOP: TRIGGER: New deployment found, updating..."
 		update $sha
 	else
-		echo "No new deployment found..."
-		if docker ps | grep agent_new
+		echo "LOOP: TRIGGER: No new deployment found..."
+		if docker ps | grep agent_new > /dev/null
 		then
-			echo "Found new agent container, renaming..."
-			docker rename agent_new agent
+			echo "LOOP: TRIGGER: Found new agent container, renaming..."
+			docker rename agent_new agent > /dev/null
 		fi
 		agent_current_sha=$(docker inspect "opensourcefoundries/agent:latest" | grep Id | sed "s/\"//g" | sed "s/,//g" |  tr -s ' ' | cut -d ' ' -f3)
 		echo $agent_current_sha > /tmp/agent.sha
+		echo "LOOP: AGENT: Checking for new agent container..."
 		update_agent $repo $branch
 		sleep 20
 	fi
